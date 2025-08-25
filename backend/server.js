e69025bf02aa21cb9feadb138647669296383fd0
Vishal0504-tech <vrvishal0504@gmail.com>
2025-08-25 19:20:08 +0530

    Fixing mistake in project

--- backend/server.js
+++ backend/server.js
@@ -189,7 +189,7 @@ io.on("connection", (socket) => {
 // ===== Serve frontend in production =====
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
